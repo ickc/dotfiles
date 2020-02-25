dd6de43ad018236398d4151bd115d1e2b17c92b3
Ka Hei Kolen Cheung <christian.kolen@gmail.com>
2020-02-25 12:06:17 -0800

    make.sh: remove NERSC old dotfiles convention

--- make.sh
+++ make.sh
@@ -8,21 +8,19 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-[[ -n "$NERSC_HOST" ]] && EXT='.ext' || EXT=
-
 # .bash_profile
 case "$__HOST" in
-    gordita|bolo) echo "exec zsh" > "$HOME/.bash_profile$EXT"          ;;
-    comet)        echo "exec $(which zsh)" > "$HOME/.bash_profile$EXT" ;;
-    *)            : > "$HOME/.bash_profile$EXT"                        ;;
+    gordita|bolo) echo "exec zsh" > "$HOME/.bash_profile"          ;;
+    comet)        echo "exec $(which zsh)" > "$HOME/.bash_profile" ;;
+    *)            : > "$HOME/.bash_profile"                        ;;
 esac
 
-cat << EOF >> "$HOME/.bash_profile$EXT"
-[[ -s "$HOME/.bashrc$EXT" ]] && . "$HOME/.bashrc$EXT"
+cat << EOF >> "$HOME/.bash_profile"
+[[ -s "$HOME/.bashrc" ]] && . "$HOME/.bashrc"
 EOF
 
 # .bashrc
-cat << EOF > "$HOME/.bashrc$EXT"
+cat << EOF > "$HOME/.bashrc"
 [[ -s "$DIR/bin/env" ]] && . "$DIR/bin/env"
 [[ -s "$DIR/bin/.env" ]] && . "$DIR/bin/.env"
 [[ -s "$DIR/bin/interactive" ]] && . "$DIR/bin/interactive"
@@ -31,31 +29,31 @@ EOF
 # assume zprezto always at ~/.zprezto
 
 # .zshenv
-cat << EOF > "$HOME/.zshenv$EXT"
+cat << EOF > "$HOME/.zshenv"
 [[ -s "$DIR/bin/env" ]] && . "$DIR/bin/env"
 [[ -s "$DIR/bin/.env" ]] && . "$DIR/bin/.env"
 [[ -s "$HOME/.zprezto/runcoms/zshenv" ]] && . "$HOME/.zprezto/runcoms/zshenv"
 EOF
 
 # .zprofile
-cat << EOF > "$HOME/.zprofile$EXT"
+cat << EOF > "$HOME/.zprofile"
 [[ -s "$HOME/.zprezto/runcoms/zprofile" ]] && . "$HOME/.zprezto/runcoms/zprofile"
 EOF
 # .zshrc
-cat << EOF > "$HOME/.zshrc$EXT"
+cat << EOF > "$HOME/.zshrc"
 [[ -s "$DIR/bin/interactive" ]] && . "$DIR/bin/interactive"
 EOF
 # only inserted in this order works
 # c.f. https://github.com/sorin-ionescu/prezto/issues/657#issuecomment-52546927
 # c.f. https://github.com/ohmyzsh/ohmyzsh/issues/7246#issuecomment-427674055
 case "$__HOST" in
-    comet) echo "export FPATH=\"$HOME/.linuxbrew/share/zsh/site-functions:$HOME/.linuxbrew/share/zsh/functions:$FPATH\"" >> "$HOME/.zshrc$EXT" ;;
+    comet) echo "export FPATH=\"$HOME/.linuxbrew/share/zsh/site-functions:$HOME/.linuxbrew/share/zsh/functions:$FPATH\"" >> "$HOME/.zshrc" ;;
 esac
-cat << EOF >> "$HOME/.zshrc$EXT"
+cat << EOF >> "$HOME/.zshrc"
 [[ -s "$HOME/.zprezto/runcoms/zshrc" ]] && . "$HOME/.zprezto/runcoms/zshrc"
 EOF
 # .zlogin
-cat << EOF > "$HOME/.zlogin$EXT"
+cat << EOF > "$HOME/.zlogin"
 [[ -s "$HOME/.zprezto/runcoms/zlogin" ]] && . "$HOME/.zprezto/runcoms/zlogin"
 EOF
 # .zlogout
