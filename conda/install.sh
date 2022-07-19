4d9afb0d2fe31a3a2f28e634ff1be744eef55d6b
Kolen Cheung <christian.kolen@gmail.com>
2022-07-18 17:16:04 -0700

    generate .condarc according to NPROCESSORS

--- conda/install.sh
+++ conda/install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/conda"
 mkdir -p "$config_dir"
-ln -sf "$DIR/.condarc" "$config_dir"
+
+rm -f "$config_dir/.condarc"
+cat << EOF > "$config_dir/.condarc"
+channels:
+    - conda-forge
+    - defaults
+channel_priority: strict
+allow_softlinks: False
+create_default_packages:
+    # - mamba
+    - pip
+default_threads: $(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
+EOF
