350d5b853889129447c2ddddf6116c63674b746d
Kolen Cheung <christian.kolen@gmail.com>
2023-04-26 19:55:46 +0100

    add centaurus

--- bin/install.sh
+++ bin/install.sh
@@ -13,11 +13,11 @@ case "$__HOST" in
     gordita|bolo)
       echo '[[ "$HOSTNAME" == gordita* ]] && SHELL="$HOME/mambaforge/envs/system39-conda-forge/bin/zsh" || SHELL="$(which zsh)"; export SHELL; [[ -e "$SHELL" ]] && exec "$SHELL" -l' > "$HOME/.bash_profile"
       ;;
-    fornax)
-      echo 'SHELL=/home/khcheung/.mambaforge/envs/system311-conda-forge/bin/zsh; export SHELL; [[ -e "$SHELL" ]] && exec "$SHELL" -l' > "$HOME/.bash_profile"
+    centaurus|fornax)
+      echo 'SHELL=/home/$USER/.mambaforge/envs/system311-conda-forge/bin/zsh; export SHELL; [[ -e "$SHELL" ]] && exec "$SHELL" -l' > "$HOME/.bash_profile"
       ;;
     simons1)
-      echo 'SHELL=/mnt/so1/users/khcheung/.mambaforge/envs/system311-conda-forge/bin/zsh; export SHELL; [[ -e "$SHELL" ]] && exec "$SHELL" -l' > "$HOME/.bash_profile"
+      echo 'SHELL=/mnt/so1/users/$USER/.mambaforge/envs/system311-conda-forge/bin/zsh; export SHELL; [[ -e "$SHELL" ]] && exec "$SHELL" -l' > "$HOME/.bash_profile"
       ;;
     *)
       : > "$HOME/.bash_profile"
