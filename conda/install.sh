1ff5af668f4875570b929c2e992a6be2d4fc8a3b
Kolen Cheung <christian.kolen@gmail.com>
2023-11-28 14:15:37 +0000

    remove defaults, pip

--- conda/install.sh
+++ conda/install.sh
@@ -7,11 +7,11 @@ rm -f "$config_dir/.condarc"
 cat << EOF > "$config_dir/.condarc"
 channels:
     - conda-forge
-    - defaults
+    # - defaults
 channel_priority: strict
 allow_softlinks: False
-create_default_packages:
+# create_default_packages:
     # - mamba
-    - pip
+    # - pip
 default_threads: $(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
 EOF
