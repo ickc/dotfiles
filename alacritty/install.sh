3f4f2ee45d9e62ef7f0fa65e837a301c5b46ef7e
Kolen Cheung <christian.kolen@gmail.com>
2024-01-03 13:59:40 +0000

    alacritty migrate

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p "${XDG_CONFIG_HOME:-${HOME}/.config}/alacritty"
-ln -sf "$DIR/alacritty.yml" "${XDG_CONFIG_HOME:-${HOME}/.config}/alacritty"
+ln -sf "$DIR/alacritty.toml" "${XDG_CONFIG_HOME:-${HOME}/.config}/alacritty"
