fd5a28f54b3e582156c9c7ff17a8394c27eb0891
Kolen Cheung <christian.kolen@gmail.com>
2024-02-07 19:02:20 +0000

    solarize to gruvbox

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Builtin Solarized Dark'
+config.color_scheme = 'GruvboxDark'
 
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
