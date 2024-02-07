b45080b9d43dc164c9163fe9d754165451b25873
Kolen Cheung <christian.kolen@gmail.com>
2024-02-07 16:49:53 +0000

    add conda, sman

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
+    if test -f ~/.mambaforge/etc/fish/conf.d/conda.fish
+        source ~/.mambaforge/etc/fish/conf.d/conda.fish
+    end
     starship init fish | source
+    if test -f ~/.sman/sman.fish
+        source ~/.sman/sman.fish
+    end
 end
