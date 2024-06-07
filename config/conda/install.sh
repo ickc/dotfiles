4b56c523ce69c8cfa7a487188396e4f03201c4d9
Kolen Cheung <christian.kolen@gmail.com>
2024-06-07 06:51:03 +0100

    remove default_threads

--- config/conda/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DEFAULT_THREADS="$(getconf _NPROCESSORS_ONLN 2> /dev/null || getconf NPROCESSORS_ONLN 2> /dev/null || echo 1)"
-export DEFAULT_THREADS
-
-while IFS= read -r line; do
-    eval echo "\"${line}\""
-done < condarc.yml > .condarc
