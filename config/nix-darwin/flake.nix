8d9de2660a4fd807867467b4289683737c3e7e29
Kolen Cheung <christian.kolen@gmail.com>
2024-06-13 16:07:56 +0100

    remove default pkgs

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -13,8 +13,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
-        ];
+        [ ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
