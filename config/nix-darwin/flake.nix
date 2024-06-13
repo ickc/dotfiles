00868be850a2ce24614595d94fea17c551b77e57
Kolen Cheung <christian.kolen@gmail.com>
2024-06-13 18:52:31 +0100

    add system.defaults

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -107,6 +107,72 @@
       # $ darwin-rebuild changelog
       system.stateVersion = 4;
 
+      system.defaults = {
+        LaunchServices.LSQuarantine = false;
+        NSGlobalDomain = {
+          AppleInterfaceStyleSwitchesAutomatically = true;
+          AppleMeasurementUnits = "Centimeters";
+          AppleMetricUnits = 1;
+          AppleShowAllExtensions = true;
+          AppleShowAllFiles = true;
+          AppleTemperatureUnit = "Celsius";
+          AppleWindowTabbingMode = "always";
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = true;
+          NSAutomaticSpellingCorrectionEnabled = false;
+          "com.apple.mouse.tapBehavior" = 1;
+          "com.apple.sound.beep.feedback" = 0;
+          "com.apple.trackpad.enableSecondaryClick" = true;
+          "com.apple.trackpad.forceClick" = true;
+        };
+        SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+        dock = {
+          autohide = true;
+          minimize-to-application = true;
+          mru-spaces = false;
+          show-recents = false;
+          wvous-bl-corner = 5;
+          wvous-tl-corner = 10;
+        };
+        finder = {
+          AppleShowAllExtensions = true;
+          AppleShowAllFiles = true;
+          FXDefaultSearchScope = "SCcf";
+          FXEnableExtensionChangeWarning = false;
+          FXPreferredViewStyle = "clmv";
+          QuitMenuItem = true;
+          ShowPathbar = true;
+          _FXShowPosixPathInTitle = false;
+        };
+        loginwindow = {
+          DisableConsoleAccess = false;
+          GuestEnabled = false;
+          PowerOffDisabledWhileLoggedIn = true;
+          RestartDisabledWhileLoggedIn = true;
+          ShutDownDisabledWhileLoggedIn = true;
+        };
+        magicmouse.MouseButtonMode = "TwoButton";
+        menuExtraClock = {
+          ShowAMPM = true;
+          ShowDate = 1;
+          ShowDayOfWeek = true;
+        };
+        screensaver = {
+          askForPassword = true;
+          askForPasswordDelay = 5;
+        };
+        spaces.spans-displays = false;
+        trackpad = {
+          ActuationStrength = 0;
+          Clicking = true;
+          TrackpadRightClick = true;
+          TrackpadThreeFingerDrag = true;
+          TrackpadThreeFingerTapGesture = 0;
+        };
+      };
+
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
