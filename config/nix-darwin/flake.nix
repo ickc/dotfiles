fe511534a6d65cb8a31441202b2ed97f1c3c2b62
Kolen Cheung <christian.kolen@gmail.com>
2024-06-13 15:50:39 +0100

    customize
    sed -i -E \
        -e "s/simple/$(scutil --get LocalHostName)/" \
        -e 's/x86_64-darwin/aarch64-darwin/' \
        flake.nix

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -35,17 +35,17 @@
       system.stateVersion = 4;
 
       # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    # $ darwin-rebuild build --flake .#Kolens-MacBook-Pro
+    darwinConfigurations."Kolens-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."simple".pkgs;
+    darwinPackages = self.darwinConfigurations."Kolens-MacBook-Pro".pkgs;
   };
 }
