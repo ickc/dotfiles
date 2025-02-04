da2dc9500a7856153ddc8ba4a67156778feb05bf
Kolen Cheung <christian.kolen@gmail.com>
2025-02-04 19:08:04 +0000

    remove nixpkgs.hostPlatform

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -417,9 +417,6 @@
               TrackpadThreeFingerTapGesture = 0;
             };
           };
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
         };
     in
     {
